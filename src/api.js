a021f36e72b66887733ed7fe25f2723e6bbd33d7
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-04-19 11:47:51 +0100

    refactor api file

--- src/api.js
+++ src/api.js
@@ -1,35 +1,31 @@
 import axios from "axios";
 
+const ncNewsApi = axios.create({
+  baseURL: "https://nc-news-npk0.onrender.com/api",
+});
+
 const fetchArticles = () => {
-  return axios
-    .get("https://nc-news-npk0.onrender.com/api/articles")
-    .then((response) => {
-      return response.data.articles;
-    });
+  return ncNewsApi.get("/articles").then((response) => {
+    return response.data.articles;
+  });
 };
 
 const fetchArticleById = (article_id) => {
-  return axios
-    .get(`https://nc-news-npk0.onrender.com/api/articles/${article_id}`)
-    .then((respose) => {
-      return respose.data.article;
-    });
+  return ncNewsApi.get(`/articles/${article_id}`).then((respose) => {
+    return respose.data.article;
+  });
 };
 
 const fetchArticleComments = (article_id) => {
-  return axios
-    .get(
-      `https://nc-news-npk0.onrender.com/api/articles/${article_id}/comments`
-    )
-    .then((response) => {
-      return response.data.comments;
-    });
+  return ncNewsApi.get(`/articles/${article_id}/comments`).then((response) => {
+    return response.data.comments;
+  });
 };
 
 const patchArticleVotes = (article_id, voteType) => {
   const voteChange = voteType === "up" ? 1 : -1;
-  return axios
-    .patch(`https://nc-news-npk0.onrender.com/api/articles/${article_id}`, {
+  return ncNewsApi
+    .patch(`/articles/${article_id}`, {
       inc_votes: voteChange,
     })
     .then((response) => {
