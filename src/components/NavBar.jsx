9d10c0817099596a410f6a2be638ed0907b4917c
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-04-21 08:34:58 +0100

    create select form to allow navigation to specific topic articles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import TopicSearch from "./TopicSearch";
 
 function NavBar() {
   return (
@@ -7,6 +8,7 @@ function NavBar() {
         <button>Home</button>
       </Link>
       <button>Users</button>
+      <TopicSearch />
     </nav>
   );
 }
