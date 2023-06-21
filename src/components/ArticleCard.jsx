b80e7b8ab33e3074ce1d4c63f6fdfa88df47f89d
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-06-21 10:01:45 +0100

    add boostrap collapsable navbar to replace topic search

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -7,7 +7,7 @@ function ArticleCard({ article }) {
         <img src={article.article_img_url} alt="article"></img>
         <div id="card-text">
           <h3>{article.title}</h3>
-          <h4>{article.author}</h4>
+          <p>{article.author}</p>
         </div>
       </li>
     </Link>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,49 @@
-import { Link } from "react-router-dom";
-import TopicSearch from "./TopicSearch";
+import { Navbar, Nav, Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
-  return (
-    <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+  const navigate = useNavigate();
 
-      <TopicSearch />
-    </nav>
+  const handleOptionClick = (topic) => {
+    navigate(`/articles?topic=${topic}`);
+  };
+
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <Navbar bg="light" expand="lg">
+      {/* <Navbar.Brand href="#">Navbar</Navbar.Brand> */}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Form inline>
+            <Button
+              variant="outline-success"
+              onClick={() => handleOptionClick("cooking")}
+            >
+              Cooking
+            </Button>
+            <Button
+              variant="outline-success"
+              onClick={() => handleOptionClick("coding")}
+            >
+              Coding
+            </Button>
+            <Button
+              variant="outline-success"
+              onClick={() => handleOptionClick("football")}
+            >
+              Football
+            </Button>
+            <Button variant="outline-primary" onClick={handleHomeClick}>
+              Home
+            </Button>
+          </Form>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
