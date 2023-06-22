c16ad924021d4a19c902b38cb183709b7ef58518
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-06-22 10:35:29 +0100

    resize offcanvas navbar menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,24 @@
 import "bootstrap/dist/css/bootstrap.css";
-import { Navbar, Nav, Form, Button, Offcanvas } from "react-bootstrap";
+import { Navbar, Nav, Button, Offcanvas } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function TopicSearch() {
   const [showOffcanvas, setShowOffcanvas] = useState(false);
   const navigate = useNavigate();
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 992);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const handleOptionClick = (topic) => {
     navigate(`/articles?topic=${topic}`);
@@ -17,85 +30,52 @@ function TopicSearch() {
     setShowOffcanvas(false); // Close the offcanvas when the Home button is clicked
   };
 
+  const renderButtons = () => {
+    return (
+      <>
+        <Button variant="outline" onClick={handleHomeClick}>
+          Home
+        </Button>
+        <Button variant="outline" onClick={() => handleOptionClick("cooking")}>
+          Cooking
+        </Button>
+        <Button variant="outline" onClick={() => handleOptionClick("coding")}>
+          Coding
+        </Button>
+        <Button variant="outline" onClick={() => handleOptionClick("football")}>
+          Football
+        </Button>
+      </>
+    );
+  };
+
   return (
     <>
       <Navbar expand="lg">
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
-          onClick={() => setShowOffcanvas(true)} // Open the offcanvas when the toggle button is clicked
+          onClick={() => setShowOffcanvas(!showOffcanvas)} // Toggle the offcanvas when the menu button is clicked
         />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <div className="d-flex">
-              {" "}
-              {/* Use div with d-flex class to display buttons side by side */}
-              <Button
-                variant="outline"
-                onClick={() => handleOptionClick("cooking")}
-              >
-                Cooking
-              </Button>
-              <Button
-                variant="outline"
-                onClick={() => handleOptionClick("coding")}
-              >
-                Coding
-              </Button>
-              <Button
-                variant="outline"
-                onClick={() => handleOptionClick("football")}
-              >
-                Football
-              </Button>
-              <Button variant="outline" onClick={handleHomeClick}>
-                Home
-              </Button>
-            </div>
-          </Nav>
+          <Nav className="mr-auto">{isMobileView ? null : renderButtons()}</Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Offcanvas
-        show={showOffcanvas}
-        onHide={() => setShowOffcanvas(false)}
-        placement="end" // Set the offcanvas placement to "end" for right-side entry
-        style={{ width: "50%" }} // Customize the width of the offcanvas
-      >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Menu</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Form>
-            <Button
-              variant="outline"
-              onClick={handleHomeClick}
-              className="d-block d-lg-none mb-2"
-            >
-              Home
-            </Button>
-            <Button
-              variant="outline"
-              onClick={() => handleOptionClick("cooking")}
-              className="d-block d-lg-none mb-2" // Hide the button on larger screens
-            >
-              Cooking
-            </Button>
-            <Button
-              variant="outline"
-              onClick={() => handleOptionClick("coding")}
-              className="d-block d-lg-none mb-2" // Hide the button on larger screens
-            >
-              Coding
-            </Button>
-            <Button
-              variant="outline"
-              onClick={() => handleOptionClick("football")}
-              className="d-block d-lg-none mb-2" // Hide the button on larger screens
-            >
-              Football
-            </Button>
-          </Form>
-        </Offcanvas.Body>
-      </Offcanvas>
+      {isMobileView && (
+        <Offcanvas
+          show={showOffcanvas}
+          onHide={() => setShowOffcanvas(false)}
+          placement="end"
+          style={{ width: "50%" }} // Set the offcanvas width to 100%
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="flex-column">{renderButtons()}</Nav>{" "}
+            {/* Render the buttons vertically */}
+          </Offcanvas.Body>
+        </Offcanvas>
+      )}
     </>
   );
 }
